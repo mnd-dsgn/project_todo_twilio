68d89c66d8808a94f4863437a5f7c9bfc5714d8f
“Matthew <matthew.hinea@gmail.com>
2016-08-13 23:19:42 -0700

    modify gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727234947) do
+ActiveRecord::Schema.define(version: 20160812210146) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "college"
+    t.string   "hometown"
+    t.string   "currently_lives"
+    t.string   "telephone"
+    t.text     "life_words"
+    t.text     "about_me"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.date     "birth_date"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
 
   create_table "tasks", force: :cascade do |t|
     t.text     "description"
     t.datetime "due_date"
     t.string   "category"
     t.integer  "priority"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.boolean  "pinned"
-    t.boolean  "completed"
+    t.boolean  "completed",   default: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "auth_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "gender"
+  end
+
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
 
 end
